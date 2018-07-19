6205b205ebaa3441e4a3611e2ca17360b339191d
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-19 12:12:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = '6686f709427783fa0050a297463e3508bd2a3895'
+
+fetch('https://api.github.com/mrkutly/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
